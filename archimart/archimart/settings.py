dd7de2e43841dc849a37888de1c2c14208c7af52
kehem <anirbansingha1@outlook.com>
2025-09-24 15:51:33 +0600

    asdfas

--- archimart/archimart/settings.py
+++ archimart/archimart/settings.py
@@ -67,7 +67,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.0.111:3000",
     "http://192.168.0.222:3000",
     "http://localhost:3000",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
 ]
 
 ROOT_URLCONF = 'archimart.urls'
